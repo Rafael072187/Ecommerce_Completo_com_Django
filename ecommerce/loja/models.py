42f69726d9409d4c46aad1e820ae59d4d9a04296
Rafael072187 <rafaelbettencourtr89@gmail.com>
2025-10-16 09:28:04 -0300

    Atualização

--- ecommerce/loja/models.py
+++ ecommerce/loja/models.py
@@ -26,12 +26,15 @@ class Tipo(models.Model):
 
 
 class Produto(models.Model):
-    imagem = models.CharField(max_length=400, null=True, blank=True)
+    imagem = models.ImageField(null=True, blank=True)
     nome = models.CharField(max_length=200, null=True, blank=True)
     preco = models.DecimalField(max_digits=10, decimal_places=2)
     ativo = models.BooleanField(default=True)
     categoria = models.ForeignKey(Categoria, null=True, blank=True, on_delete=models.SET_NULL)
     tipo = models.ForeignKey(Tipo, null=True, blank=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f"Nome: {self.nome}, Categoria: {self.categoria}, Tipo: {self.tipo}, Preco: {self.preco}"
 
 
 class ItemEstoque(models.Model):
@@ -63,3 +66,12 @@ class ItensPedido(models.Model):
     item_estoque = models.ForeignKey(ItemEstoque, null=True, blank=True, on_delete=models.SET_NULL)
     quantidade = models.IntegerField(default=0)
     pedido = models.ForeignKey(Pedido, null=True, blank=True, on_delete=models.SET_NULL)
+
+
+class Banner(models.Model):
+    imagem = models.ImageField(null=True, blank=True)
+    link_destino = models.CharField(max_length=400, null=True, blank=True)
+    ativo = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.link_destino} - Ativo: {self.ativo}"
--- ecommerce/loja/views.py
+++ ecommerce/loja/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 def homepage(request):
-    return render(request, 'homepage.html')
+    banners = Banner.objects.filter(ativo=True)
+    context = {"banners": banners}
+    return render(request, 'homepage.html', context)
 
 
-def loja(request):
-    return render(request, 'loja.html')
+def loja(request, nome_categoria=None):
+    produtos = Produto.objects.filter(ativo=True)
+    if nome_categoria:
+        produtos = produtos.filter(categoria__nome=nome_categoria)
+    context = {"produtos": produtos}
+    return render(request, 'loja.html', context)
+
+
+def ver_produto(request, id_produto):
+    produto = Produto.objects.get(id=id_produto)
+    context = {"produto":produto}
+    return render(request, "ver_produto.html", context)
 
 
 def carrinho(request):
