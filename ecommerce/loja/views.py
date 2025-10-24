4447682555bae393e023d2e798048d9a2805d37a
Rafael072187 <rafaelbettencourtr89@gmail.com>
2025-10-24 10:10:00 -0300

    deploy

--- ecommerce/loja/views.py
+++ ecommerce/loja/views.py
@@ -10,11 +10,32 @@ def homepage(request):
     return render(request, 'homepage.html', context)
 
 def loja(request, filtro=None):
+
     produtos = Produto.objects.filter(ativo=True)
     produtos = filtrar_produtos(produtos, filtro)
-    tamanhos = ["P", "M", "G"]
+    
+    if request.method == "POST":
+        dados = request.POST.dict()
+        produtos = produtos.filter(preco__gte=dados.get("preco_minimo"), preco__lte=dados.get("preco_maximo"))
+        if "tamanho" in dados:
+            itens = ItemEstoque.objects.filter(produto__in=produtos, tamanho=dados.get("tamanho"))
+            ids_produtos = itens.values_list("produto", flat=True).distinct()
+            produtos = Produto.objects.filter(id__in=ids_produtos)
+
+        if "tipo" in dados:
+            produtos = produtos.filter(tipo__slug=dados.get("tipo"))
+        
+        if "categoria" in dados:
+            produtos = produtos.filter(categoria__slug=dados.get("categoria"))
+
+    itens = ItemEstoque.objects.filter(quantidade__gt=0, produto__in=produtos)
+    tamanhos = itens.values_list("tamanho", flat=True).distinct()
+    ids_categorias = produtos.values_list("categoria", flat=True).distinct()
+    categorias = Categoria.objects.filter(id__in=ids_categorias)
+
     minimo, maximo = preco_minimo_maximo(produtos)
-    context = {"produtos": produtos, "minimo": minimo, "maximo": maximo, "tamanhos":tamanhos}
+    context = {"produtos": produtos, "minimo": minimo, "maximo": maximo, "tamanhos":tamanhos, 
+               "categorias":categorias}
     return render(request, 'loja.html', context)
 
 def ver_produto(request, id_produto, id_cor=None):
